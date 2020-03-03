5188cf3ffd0f5dbe5dcd8a870afc1b5327a6ceeb
John Oblenda <joandong2@gmail.com>
2020-03-03 18:22:52 -0500

    ..

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-import ApodCard from "./components/ApodCard.js";
+import Data from "./components/Data.js";
 
 function App() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
         axios
-            .get(
-                "https://api.nasa.gov/planetary/apod?api_key=CMQJWBxUfFBQ9MXRCtwZkzEcr6psRLcYnh50DHJq"
-            )
+            .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
             .then((response) => {
+                console.log(response);
                 setData([response.data]);
             })
             .catch((err) => console.log(err));
@@ -22,11 +21,12 @@ function App() {
         <div className="App">
             {data.map((item, i) => {
                 return (
-                    <ApodCard
+                    <Data
                         key={i}
                         title={item.title}
                         explanation={item.explanation}
                         media_type={item.media_type}
+                        video_url={item.url}
                     />
                 );
             })}
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const ApodCard = (props) => {
+const Data = (props) => {
     return (
         <div className="apod-card">
             <h2>Film title: {props.title}</h2>
             <p>{props.explanation}</p>
             <p>{props.media_type}</p>
+            <video src={props.video_url} controls></video>
         </div>
     );
 };
-export default ApodCard;
+export default Data;
