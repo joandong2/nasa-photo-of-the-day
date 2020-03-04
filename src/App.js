cc4e1197b0011daa44b82302735d68259a33b20f
John Oblenda <joandong2@gmail.com>
2020-03-04 10:11:38 -0500

    fixed video render

--- src/App.js
+++ src/App.js
@@ -6,19 +6,32 @@ import Data from "./components/Data.js";
 
 function App() {
     const [data, setData] = useState([]);
+    const [date, setDate] = useState("");
+
+    const changeHandler = (e) => {
+        console.log(e.target.value);
+        console.log(new Date(e.target.value));
+        console.log(new Date(Date.now()));
+        setDate(e.target.value);
+    };
 
     useEffect(() => {
         axios
-            .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+            .get(
+                `https://api.nasa.gov/planetary/apod?api_key=CMQJWBxUfFBQ9MXRCtwZkzEcr6psRLcYnh50DHJq&date=${date}`
+            )
             .then((response) => {
                 console.log(response);
                 setData([response.data]);
             })
             .catch((err) => console.log(err));
-    }, []);
+    }, [date]);
 
     return (
         <div className="App">
+            <form>
+                <input type="date" value={date} onChange={changeHandler} />
+            </form>
             {data.map((item, i) => {
                 return (
                     <Data
@@ -26,7 +39,7 @@ function App() {
                         title={item.title}
                         explanation={item.explanation}
                         media_type={item.media_type}
-                        video_url={item.url}
+                        url={item.url}
                     />
                 );
             })}
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 const Data = (props) => {
+    let element = null;
+    switch (props.media_type) {
+        case "image":
+            element = <img src={props.url} alt="adop_image" />;
+            break;
+        case "video":
+            element = (
+                <iframe
+                    title="apod_video"
+                    width="420"
+                    height="315"
+                    src={props.url}
+                />
+            );
+            break;
+        default:
+            break;
+    }
+
     return (
         <div className="apod-card">
             <h2>Film title: {props.title}</h2>
             <p>{props.explanation}</p>
-            <p>{props.media_type}</p>
-            <video src={props.video_url} controls></video>
+            {element}
         </div>
     );
 };
